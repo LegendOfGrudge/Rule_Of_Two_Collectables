b7513f7c44bb34edd1165d770458a83ded873253
Pamplemousse47 <agentq47@gmail.com>
2023-11-24 14:15:34 -0600

    added an about section

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,4 +1,17 @@
 class AboutController < ApplicationController
   def index
+    @collections = Collection.includes(toys: { collection: :brand })
+    @random_toy_images = {}
+
+    @collections.each do |collection|
+      random_toy = collection.toys.order(Arel.sql('RANDOM()')).first
+      if random_toy
+        brand_name = collection.brand&.name || 'N/A'
+        @random_toy_images[collection.name] = {
+          image: random_toy.image,
+          brand_name: brand_name
+        }
+      end
+    end
   end
 end
