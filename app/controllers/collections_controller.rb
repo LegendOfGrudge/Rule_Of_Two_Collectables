6b8680ac531158b0995bef1ce8081ac66dfc25fe
Nick <nicholasfletcher3682@yahoo.ca>
2023-11-24 14:25:07 -0600

    Added Collection index and show pages

---
+++ app/controllers/collections_controller.rb
@@ -0,0 +1,10 @@
+class CollectionsController < ApplicationController
+  def index
+    @collections = Collection.all
+  end
+
+  def show
+    @collection = Collection.find(params[:id])
+    @toys = Toy.where(:collection_id => params[:id]).order("name ASC")
+  end
+end
