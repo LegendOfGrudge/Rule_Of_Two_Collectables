33dd6be38511bfbe3cf59f0fcf48de431f2376bf
Nick <nicholasfletcher3682@yahoo.ca>
2023-11-28 14:31:04 -0600

    Adjusted search to use categories

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :set_brands
+  before_action :search_collections
   def set_brands
     @brands = Brand.all
+  end
+  def search_collections
+    @collections = Collection.all
   end
 end
