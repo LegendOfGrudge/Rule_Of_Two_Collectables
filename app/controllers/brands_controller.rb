7591102f0dd93b04541e79ff74fdbf3947ff78c6
Pamplemousse47 <AgentQ47@gmail.com>
2023-11-28 14:29:19 -0600

    developed the brands page

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -5,6 +5,13 @@ class BrandsController < ApplicationController
 
   def show
     @brand = Brand.find(params[:id])
-    @collections = Collection.where(:brand_id => params[:id])
+    @collections = Collection.where(brand_id: @brand.id).includes(:toys)
+
+    @top_toys = []
+    @collections.each do |collection|
+      top_toys_collection = collection.toys.order(quantity: :desc).limit(5)
+      @top_toys.concat(top_toys_collection)
+    end
+    @top_toys = @top_toys.sort_by { |toy| -toy.quantity }
   end
 end
