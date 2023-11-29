4d684560e4d2b70a74bc620b12853e811fb2937a
Pamplemousse47 <AgentQ47@gmail.com>
2023-11-28 20:46:03 -0600

    added a sorting function to the collections

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -5,6 +5,26 @@ class CollectionsController < ApplicationController
 
   def show
     @collection = Collection.find(params[:id])
-    @toys = Toy.where(:collection_id => params[:id]).order("quantity DESC").page params[:page]
+    @toys = Toy.where(collection_id: params[:id])
+
+    case params[:sort_by]
+    when 'alphabetical_asc'
+      @toys = @toys.order(name: :asc)
+    when 'alphabetical_desc'
+      @toys = @toys.order(name: :desc)
+    when 'price_desc'
+      @toys = @toys.order(price: :desc)
+    when 'price_asc'
+      @toys = @toys.order(price: :asc)
+    when 'quantity_desc'
+      @toys = @toys.order(quantity: :desc)
+    when 'quantity_asc'
+      @toys = @toys.order(quantity: :asc)
+    else
+      # Default sorting if no option is selected
+      @toys = @toys.order(quantity: :desc)
+    end
+
+    @toys = @toys.page(params[:page])
   end
 end
