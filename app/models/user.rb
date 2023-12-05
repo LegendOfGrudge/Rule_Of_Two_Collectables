92bba3db24d486a486233f15386081bf3474db2c
Nick <nicholasfletcher3682@yahoo.ca>
2023-12-05 09:32:53 -0600

    Added Province to User table and sign up page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,15 @@
 class User < ApplicationRecord
+  belongs_to :province
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   def self.ransackable_attributes(auth_object = nil)
-    ["created_at", "email", "encrypted_password", "id", "name", "remember_created_at", "reset_password_sent_at", "reset_password_token", "updated_at"]
+    ["address", "created_at", "email", "encrypted_password", "id", "name", "province", "remember_created_at", "reset_password_sent_at", "reset_password_token", "updated_at"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    ["province"]
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_05_032944) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_05_040823) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -93,11 +93,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_05_032944) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.string "address"
+    t.integer "province_id", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["province_id"], name: "index_users_on_province_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "collections", "brands"
   add_foreign_key "toys", "collections"
   add_foreign_key "toys", "toy_types"
+  add_foreign_key "users", "provinces"
 end
