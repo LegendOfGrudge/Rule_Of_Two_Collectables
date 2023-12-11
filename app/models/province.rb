aad03c45201e2d99f319c0467d9a7c02c3222533
Nick <nicholasfletcher3682@yahoo.ca>
2023-12-11 12:14:11 -0600

    Changed Provinces to be included in Active Admin

--- app/models/province.rb
+++ app/models/province.rb
@@ -4,4 +4,8 @@ class Province < ApplicationRecord
   def self.ransackable_attributes(auth_object = nil)
     ["abbr", "created_at", "gst", "hst", "id", "name", "pst", "updated_at"]
   end
+
+  def self.ransackable_associations(auth_object = nil)
+    ["users"]
+  end
 end
