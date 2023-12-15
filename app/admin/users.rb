39039d1d08110c430d518995506c625e390346d2
Nick <nicholasfletcher3682@yahoo.ca>
2023-12-15 12:38:30 -0600

    Modified mobile view to shrink image and create hamburger menu

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,11 +1,11 @@
 ActiveAdmin.register User do
-
+  remove_filter :order_id
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  # permit_params :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :name
+  permit_params :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :name
   #
   # or
   #
@@ -14,5 +14,5 @@ ActiveAdmin.register User do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
-  
+
 end
