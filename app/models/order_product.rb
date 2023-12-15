f3b940409f4053843fd68c43cb8398314cdf4cb9
Nick <nicholasfletcher3682@yahoo.ca>
2023-12-15 13:26:03 -0600

    Added extra validations

--- app/models/order_product.rb
+++ app/models/order_product.rb
@@ -2,8 +2,9 @@ class OrderProduct < ApplicationRecord
   belongs_to :toy
   belongs_to :order
 
-  validates :product_cost, :product_quantity, presence: true
-  validates :product_cost, :product_quantity, numericality: true
+  validates :product_cost, :product_quantity, :toy_id, :order_id, presence: true
+  validates :product_cost, :product_quantity, :toy_id, :order_id, numericality: true
+  validates :product_cost, :product_quantity, :toy_id, :order_id, numericality: { only_integer: true }
 
   def self.ransackable_attributes(auth_object = nil)
     ["created_at", "id", "order_id", "product_cost", "product_quantity", "toy_id", "updated_at"]
